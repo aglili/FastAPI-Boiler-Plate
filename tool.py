f9c4c152cd5b2b030c9e4070f509611dff1b61dc
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-05-21 23:06:41 +0000

    chore: cli tool script

--- tool.py
+++ tool.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
-import os 
+import os
 import shutil
 
 BOILERPLATE_PATH = os.path.join(os.path.dirname(__file__), "app")
+
 
 def init():
     """
     Initializes a new FastAPI project with the given project name.
 
-    This function prompts the user to enter a project name and creates a new folder with that name in the current working directory. If a folder with the same name already exists, it prints a message and returns.
+    This function prompts the user to enter a project name and creates a new folder
+    with that name in the current working directory. If a folder with the same name
+    already exists, it prints a message and returns.
 
-    The function then copies all the files and directories from the `BOILERPLATE_PATH` directory to the newly created project folder.
+    The function then copies all the files and directories from
+    the `BOILERPLATE_PATH` directory to the newly created project folder.
 
     Parameters:
         None
@@ -19,17 +23,17 @@ def init():
     Returns:
         None
     """
-    
+
     print("Initialize your FastAPI project")
     project_name = input("Project Name: ")
-    
+
     folder = os.path.join(os.getcwd(), project_name)
     if os.path.exists(folder):
         print("A folder with such name already exists")
         return
 
     os.makedirs(folder)
-    
+
     for root, dirs, files in os.walk(BOILERPLATE_PATH):
         for file in files:
             src = os.path.join(root, file)
@@ -38,6 +42,7 @@ def init():
             shutil.copy(src, directory)
 
     print("Project created successfully")
+
 
 if __name__ == "__main__":
     init()
